8ecd312fb3f0b515a65371e483cabe41ef044538
Omar-Karimov <omarkerimov303@gmail.com>
2024-04-11 20:18:32 +0400

    Update setup.py file

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="AI4Renewals",
+    version="0.0.0",
+    author="Omar-Karimov",
+    author_email="omarkerimov303@gmail.com",
+    packages=find_packages()
+)
